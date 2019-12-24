0e4e711969891c61d701a109732016c481990158
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-12-24 15:56:46 +0200

    Initial Commit

---
+++ delgation -in-tab-bar-controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delgation -in-tab-bar-controllers
+//
+//  Created by Mohamed Samir on 12/24/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
