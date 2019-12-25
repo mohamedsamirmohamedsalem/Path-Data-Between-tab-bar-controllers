3bcb0962a4ec398835db8afeccb83512b4b19c58
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-12-25 13:34:10 +0200

    Done

---
+++ delgation -in-tab-bar-controllers/secondVC.swift
@@ -0,0 +1,32 @@
+//
+//  secondvc.swift
+//  delgation -in-tab-bar-controllers
+//
+//  Created by Mohamed Samir on 12/24/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class secondVC: UIViewController {
+
+    @IBOutlet var secondTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
